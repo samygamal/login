62b500941e36192f184bb82231a0c3a4eb97d514
samygamal <32732279+samygamal@users.noreply.github.com>
2017-10-18 13:40:22 +0200

    Add files via upload

---
+++ log.js
@@ -0,0 +1,67 @@
+/*
+samy2081996
+*/
+
+
+//الحلقه 40
+var sam;
+sam=function(){
+	"use strict";
+	var d,
+		
+		s;
+
+	d= document.getElementById("x");
+	s=document.getElementById("in");
+	d.innerHTML="<center aria-bottom>"+s.value;
+	
+		
+		
+
+
+};
+var sam2;
+sam2=function(){
+	"use strict";
+	var d2,
+		s2;
+	d2= document.getElementById("x2");
+	s2=document.getElementById("in2");
+	d2.innerHTML="<center>"+s2.value;
+
+};
+var sam3;
+sam3=function(){
+	
+		"use strict";
+	
+	var d3,
+	
+		s3;
+		if(s3===1){d3.innerHTML="you press number one";}
+	
+
+	
+		d3= document.getElementById("x3");
+	s3=document.getElementById("in3");
+	
+		d3.innerHTML="<center>"+s3.value;
+	
+
+	
+	
+	
+
+};
+
+/*
+
+الحلقه 45
+الحلقه 50
+تم بحمد الله الانتهاء من لغة الجافا سكريبت نهائيا وبلا رجعه
+complite 100% الحلقه رقم 65
+
+*/
+
+
+
---
+++ log.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>samy gamal</title>
+</head>
+<link rel="alternate stylesheet" href="log.css">
+<body>
+	<script type="text/jscript">
+	
+	alert("ميه مسا أمعلمين الويب");
+	
+ prompt("أسم الشبح",alert("اكتب أسمك يازوق"));
+ alert("نورت يانجم");
+
+	</script>
+	
+<center>
+<div id="sss"></div>
+<button onClick="sam()">check up</button>
+<input type="text" id="in" placeholder="inter your name" autocomplete="off">
+<br><br><br>
+<div id="x"></div>
+<button onClick="sam2()">check up</button>
+<input type="text" id="in2" placeholder="inter your age">
+<br><br><br>
+<br><div id="x2"></div>
+<button onClick="sam3()">check up</button>
+<input type= "password" id="in3" placeholder="enter your password">
+<br><br>
+<div id="x3"></div>
+<font color="#159C00" size="+3" id="fo"></font>
+</center>
+
+<?php
+	
+	echo"<div style='width: 350px; height: 400; background-color: #3D00A0'>";
+	echo"<div style='width: 350px; height: 400; background-color: #3D00A0'>";
+
+$s="mohamed";
+
+	echo "<font size='+5' color='#DFDFDF'>
+".$s;
+	$m = "<font size='+5' color='#DFDFDF'>
+"."<br>"."ahmed";
+	echo $m;
+	$e="hossine";
+	$x="firas"."<br>";
+	$x.="samy"."<br>"."mahmoud";
+	echo"<br>".$e."<br>".$x."<br>";
+	echo $x.="<br>"."sameh";
+	echo"</div>";
+	echo"<br>".$e."<br>".$m;
+	echo "<br>";
+	define("SAM","SAMYGAMAL",true);
+	echo "<font color=green>".sam;
+		?>
+		<?php
+	$my=20;
+	$your=20;
+	$my+=$your;
+	$my+=300;
+
+	echo $my."you incriz"."300";
+	//الحلقه 14 أعاده للتأكيد
+	//الحلقه 17 لظروف خاصه
+	
+	?>
+	
+	
+
+</body>
+<script src="log.js" type="text/jscript"></script>
+</html>
